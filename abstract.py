e37e6e512dbebf85ff3d4eca1205d962e7a3e376
vishwassde-del <vishwassde-del>
2025-10-30 22:01:30 +0530

    Initial commit

---
+++ abstract.py
@@ -0,0 +1,8 @@
+a=int(input("enter a number :"))
+b=int(input("enter a number :"))
+try:
+    print(a/b)
+except Exception as e:
+    print("errror occured...")
+else:
+    print("pgm executed successfully")
