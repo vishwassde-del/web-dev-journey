93f9deb41c99440e1b40ba720f3da166ce8f567c
vishwassde-del <vishwas.sde@gmail.com>
2025-11-11 10:19:01 +0530

    daily commit

---
+++ conditionalexample/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Condition1 from './Condition1'
+import Condition2 from './Condition2'
+import Condition3 from './Component3'
+
+const App = () => {
+  return (
+    <div>
+      <Condition1/>
+      <Condition2/>
+      <Condition3/>
+    </div>
+  )
+}
+
+export default App
