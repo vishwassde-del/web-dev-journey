a7eeb8d08b19cb6ace4439422ef91de0d5525f3b
vishwassde-del <vishwas.sde@gmail.com>
2025-11-25 12:01:00 +0530

    daily uptate

--- learningreact/src/App.jsx
+++ learningreact/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import RuseState from './RuseState'
+import Rmap from './Rmap'
 
 const App = () => {
   return (
     <div>
       <RuseState/>
+      <Rmap/>
     </div>
   )
 }
---
+++ learningreact/src/Rmap.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Rmap = () => {
+  
+    let movies=["uhudah","uyue","wuekyf"]
+    let players=["vk","msd","st"]
+    let fruits=["ehbc","eueb","uybew"]
+    let isLoggedIn=true
+    return(
+        <>
+        <center><h2>(MAPPING):</h2>
+        <ul style={{listStyle:"none"}}>
+            {movies.map((movie,index)=>(<li key={index}>{movie}</li>))}
+        </ul>
+        <ul style={{listStyle:"none"}}>
+            {players.map((player,index)=>(<li key={index}>{player}</li>))}
+        </ul>
+        <ul style={{listStyle:"none"}}>
+            {fruits.map((fruit,index)=><li key={index}>{fruit}</li>)}
+        </ul>
+        <img src='/' alt='xyz' style={{width:"100px"}} className='images'></img>
+
+        {/*ternary*/}
+        {isLoggedIn?<h2>Welecome back</h2>:<h2>Please Log In</h2>}
+        </center>
+
+        </>
+    )
+  
+}
+
+export default Rmap
