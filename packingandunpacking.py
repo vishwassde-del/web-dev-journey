615aa8b224ceb613e14397bcd8fff4f752666093
vishwassde-del <vishwas.sde@gmail.com>
2025-12-14 17:33:54 +0530

    daily commit

---
+++ packingandunpacking.py
@@ -0,0 +1,3 @@
+s="rama"#packing(def packing : multiple characters to a single variable)
+a,b,c,d=s#unpacking(def  unpacking : assigining each characters of a string to individual variables)
+print(a,b,c,d)
